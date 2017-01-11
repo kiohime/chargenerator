315cfcc796ca2bb770a73d8fd8b2bb999286400b
kiohime <kiohere@gmail.com>
2017-01-11 13:20:47 +1000

    some shit

--- main.go
+++ main.go
@@ -21,6 +21,23 @@ import (
 // 	return
 // }
 
+// readLines reads a whole file into memory
+// // and returns a slice of its lines.
+// func readLines(path string) ([]string, error) {
+// 	file, err := os.Open(path)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	defer file.Close()
+
+// 	var lines []string
+// 	scanner := bufio.NewScanner(file)
+// 	for scanner.Scan() {
+// 		lines = append(lines, scanner.Text())
+// 	}
+// 	return lines, scanner.Err()
+// }
+
 func main() {
 	readName, _ := ioutil.ReadFile("charNames.txt")
 	// if err != nil {
